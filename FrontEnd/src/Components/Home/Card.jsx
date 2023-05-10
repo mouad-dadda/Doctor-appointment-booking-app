c559cc1bc6e85ac5da6d0e1a1b7cce8345592e1c
Kaw_kawa03 <102548978+Skawtar@users.noreply.github.com>
2023-05-10 12:06:06 +0100

    Update Card.jsx

--- FrontEnd/src/Components/Home/Card.jsx
+++ FrontEnd/src/Components/Home/Card.jsx
@@ -1,25 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 import '../../Assets/Css/HomeCss/Card.css'
+
 const Card = (props) => {
+  const [doctors, setDoctors] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/api/doctors')
+      .then(response => {
+        const doctorsData = response.data;
+        setDoctors(doctorsData);
+      })
+      .catch(error => {
+       console.error( error);
+      });
+  }, []);
+
   return (
     <>
-      <div className="card_body">
-        <div>
-          <img src={props.img} alt=""  style={{display:"initial"}} />
+      {doctors.map(doctor => (
+        <div className="card_body" key={doctor.id}>
+          <div>
+            <img src={doctor.img} alt="" style={{ display: "initial" }} />
+          </div>
+          <h1>{doctor.name}</h1>
+          <p>{doctor.qualifications}</p>
+          <div className="card_btn">
+            <button className="btn_card mr_ri btn_bg_primary ">
+              <a href="/reserve">Reserve</a>
+            </button>
+            <button className="btn_card mr_lf btn_border_primary">
+              View Profile
+            </button>
+          </div>
         </div>
-        <h1>Paul Richard</h1>
-        <p>
-          MBBS, MD - Dermatologie, vénéréologie et lèpre Réservez notre médecin
-        </p>
-        <div className="card_btn">
-          <button className="btn_card mr_ri btn_bg_primary ">
-            <a href="/reserve">Reserve</a>
-          </button>
-          <button className="btn_card mr_lf btn_border_primary">
-            View Profile
-          </button>
-        </div>
-      </div>
+      ))}
     </>
   );
 };

9e0ffbc2653f2dafa8e64fdcb8b67cd088d34290
mouad-dadda <mouaddadda7@gmail.com>
2023-06-04 20:49:05 +0000

    Add Lang ' EN , FR , CN '

--- FrontEnd/src/i18n.js
+++ FrontEnd/src/i18n.js
@@ -4,30 +4,12 @@ import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-import transEn from "./lang/en.json";
-import transFr from "./lang/fr.json";
-import transCn from "./lang/cn.json";
-
-
-const resources = {
-  en: {
-    translation: transEn
-  },
-  fr: {
-    translation: transFr
-  },
-  cn: {
-    translation: transCn
-  },
-};
-
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources,
-    supportedLngs: ["en", "fr","cn"],
+    supportedLngs: ["en", "fr", "cn"],
     fallbackLng: "en",
     debug: true,
     detection: {
